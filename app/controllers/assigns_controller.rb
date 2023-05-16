9435fddf788f1700d85bab5e22a6a47033bc3553
she <fangfangshe09@gmail.com>
2023-05-16 12:12:45 +0900

    Warning を解消しました

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -22,6 +22,7 @@ class AssignsController < ApplicationController
   end
 
   private
+
   def assign_params
     params[:email]
   end
@@ -42,7 +43,7 @@ class AssignsController < ApplicationController
   def email_exist?
     team = find_team(params[:team_id])
     if team.members.exists?(email: params[:email])
-      redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
+        redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
     end
   end
 
@@ -62,7 +63,7 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  def find_team(team_id)
+  def find_team(_team_id)
     Team.friendly.find(params[:team_id])
   end
 end
