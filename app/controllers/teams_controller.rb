30ec0a24671094d50e7a9a4f60afeb3260713ab2
she <fangfangshe09@gmail.com>
2023-05-19 17:48:10 +0900

    もう一回修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -53,6 +53,7 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+
   private
 
   def set_team
