0fe296f647b761e1c11b25c9684810dd7ed2ad33
she <fangfangshe09@gmail.com>
2023-05-20 09:51:48 +0900

    アジェンダの削除機能を実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -24,7 +24,7 @@ class AgendasController < ApplicationController
   def destroy 
     @agenda.destroy
     if @agenda.user_id == current_user.id || @agenda.team.owner_id == current_user.id
-    redirect_to agendas_url, notice: I18n.t('views.messages.delete_agenda')
+    redirect_to dashboard_url, notice: I18n.t('views.messages.delete_agenda')
     end
   end
 
